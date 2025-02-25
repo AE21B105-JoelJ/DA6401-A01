c1eb8243a4b3064c7ee29104e77fee284be8b39f
AE21B105-JoelJ <ae21b105@smail.iitm.ac.in>
2025-02-25 15:27:29 +0530

    added random and Xavier init

--- source.py
+++ source.py
@@ -2,19 +2,30 @@
 from typing import List
 import numpy as np
 
-def init_zero_mat(Info : List[int]):
+def init_zero_mat(Info : List[int], init_scheme = "random"):
     """
     This functions gets input of the sequence of choises for the hidden layer and neurons per layer and output zero initialized matrices.
     Info : List (Example : [20, 50, 50, 10] Tells that input : 20, first hidden layer : 50, second hidden layer : 50 and output layer : 10) 
     Returns :
     List[numpy.ndarray], List[numpy.ndarray] : 2 List of numpy arrays oe for weights and one for biases.
     """
-
+    init_scheme_all = ["Xavier", "random"]
+    if init_scheme not in init_scheme_all:
+        raise Exception("The Initializations scheme is either not valid or not appropriate !!!")
+    
     Weights, Biases = [], []
     input = Info[0] # Input 
     for i in range(1,len(Info)):
-        weight_matrix = np.zeros(shape=(Info[i],Info[i-1])) # Creating weight matrix for each layer
-        bias_matrix = np.zeros(shape=(Info[i],1)) # Creating bias matrix for each layer
+        if init_scheme == "random":
+            # Here we consider uniformly random from [-1,1]
+            weight_matrix = np.random.uniform(low = -1.0, high = 1.0, size=(Info[i],Info[i-1])) # Creating weight matrix for each layer
+            bias_matrix = np.random.uniform(low = -1.0, high = 1.0, size=(Info[i],1)) # Creating bias matrix for each layer
+
+        elif init_scheme == "Xavier":
+            input_output = Info[i-1] + Info[i]
+            std = np.sqrt(2/input_output)
+            weight_matrix = np.random.randn(Info[i], Info[i-1])*std # Creating weight matrix for each layer
+            bias_matrix = np.random.randn(Info[i], 1)*std # Creating bias matrix for each layer
 
         # Append the weight and bias matrix ot original list #
         Weights.append(weight_matrix)
