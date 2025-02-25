c22de33b524a2e6825ed81c48edbef56d595986b
AE21B105-JoelJ <ae21b105@smail.iitm.ac.in>
2025-02-25 14:57:26 +0530

    init function added

--- source.py
+++ source.py
@@ -1,1 +1,13 @@
 # Source File (Thought to make the core algorithms) #
+from typing import List
+import numpy as np
+
+def init_zero_mat(Info : List):
+    """
+    This functions gets input of the sequence of choises for the hidden layer and neurons per layer and output zero initialized matrices.
+    Info : List (Example : [20, 50, 50, 10] Tells that input : 20, first hidden layer : 50, second hidden layer : 50 and output layer : 10) 
+    Returns :
+    List[numpy.ndarray], List[numpy.ndarray] : 2 List of numpy arrays oe for weights and one for biases.
+    """
+
+    
