c0106badae699b57b42ce933baa18d82a635da50
AE21B105-JoelJ <ae21b105@smail.iitm.ac.in>
2025-03-03 17:30:49 +0530

    decoupled batchloader and added forward call in model

--- source.py
+++ source.py
@@ -227,7 +227,7 @@ def batchloader(X_data, y_data, batch_size = 32, shuffle = True):
         batches_x.append(X_data[i*batch_size:(i+1)*batch_size])
         batches_y.append(y_data[i*batch_size:(i+1)*batch_size])
     # returning a zip of the batch
-    return zip(batches_x, batches_y)
+    return batches_x, batches_y
 
 class Optimizer:
     def __init__(self, loss = "mean_squared_error", optimizer = "gd", learning_rate = 0.001, momentum = 0):
@@ -392,5 +392,18 @@ class FeedForwardNeuralNetwork:
         # Initialization of the weights
         self.weights, self.biases = init_mat(Info=self.arch, init_scheme= self.initialization)
 
-    def forward_call():
-        pass
+    def forward_call(self, inputs_, is_batchloaded = False):
+        if is_batchloaded:
+            outputs_list = []
+            for X in inputs_:
+                out_batch, _, _ = forward_propagation(X, self.weights, self.biases, activation_sequence=self.activation_seqence)
+                outputs_list.append(out_batch)
+            outputs_ = outputs_list[0]
+            for i in range(1,len(outputs_list)):
+                outputs_ = np.append(outputs_, outputs_list[i])
+        else:
+            outputs_, _, _ = forward_propagation(inputs_, self.weights, self.biases, activation_sequence=self.activation_seqence)
+
+        return outputs_
+    
+    
